a7e794dce93c47b4034734389f7f1fde36243d6c
Wiktor Sołtys <wiktorsoltys@gmail.com>
2021-11-10 12:12:50 +0100

    initial commit- project

---
+++ ProgramDlaOli/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ProgramDlaOli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ZarzadaniePlikiem plik = new ZarzadaniePlikiem();
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            plik.NazwaPliku3 = "PlikDlaOlenki.txt";
+            
+        }
+
+        private void BtnWybierz1_Click(object sender, RoutedEventArgs e)
+        {
+            plik.NazwaPliku1 = WyborPliku();
+            if (!(plik.NazwaPliku1 == null))
+            {
+                tbPlik1.Text = plik.NazwaPliku1;
+            }
+        }
+
+        private void BtnWybierz2_Click(object sender, RoutedEventArgs e)
+        {
+            plik.NazwaPliku2 = WyborPliku();
+            if (!(plik.NazwaPliku2 == null))
+            {
+                tbPlik2.Text = plik.NazwaPliku2;
+            }
+        }
+
+        private void BtnWybierz3_Click(object sender, RoutedEventArgs e)
+        {
+            string folderDocelowy=null;
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                folderDocelowy = dialog.FileName;
+            }
+            if (!(folderDocelowy == null))
+            {
+                plik.NazwaPliku3 = string.Concat(folderDocelowy, "\\PlikDlaOlenki.txt");
+                tbPlik3.Text = plik.NazwaPliku3;
+            }
+        }
+
+        private string WyborPliku()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            Nullable<bool> dialogOK = fileDialog.ShowDialog();
+            if (dialogOK == true)
+            {
+                return fileDialog.FileName;
+            }
+            return null;
+        }
+
+        private void BtnWykonaj_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (plik.ZaczytajDane())
+            {
+                plik.Znajdz();
+                tbStatus.Text = "Wykonano poprawnie i zapisano pod nazwą PlikDlaOlenki";
+            }
+            else
+            {
+                tbStatus.Text = "Załaduj poprawnie pliki";
+            }
+        }
+    }
+}
---
+++ ProgramDlaOli/ZarzadaniePlikiem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDlaOli
+{
+    public class ZarzadaniePlikiem
+    {
+        List<string> lista = new List<string>();
+        public string NazwaPliku1 { get; set; }
+        public string NazwaPliku2 { get; set; }
+        public string NazwaPliku3 { get; set; }
+
+        public string[] plik1;
+        public string[] plik2;
+        public string[] plik3;
+
+        public bool ZaczytajDane()
+        {
+            if (!File.Exists(NazwaPliku1))
+            {
+                return false;
+            }
+            plik1 = File.ReadAllLines(NazwaPliku1);
+            
+            if (!File.Exists(NazwaPliku2))
+            {
+                return false;
+            }
+            plik2 = File.ReadAllLines(NazwaPliku2);
+
+            return true;
+
+        }
+
+        public void Znajdz()
+        {
+            WysczyscListe();
+            int pozycjaTab=0;
+            foreach(var liniePliku1 in plik1)
+            {
+                pozycjaTab=liniePliku1.IndexOf("\t");
+                if (pozycjaTab >= 0)
+                {
+                    string NumerSeryjnyCzesci = liniePliku1.Substring(0, pozycjaTab);
+                    foreach(var liniePliku2 in plik2)
+                    {
+                        if (NumerSeryjnyCzesci.Equals(liniePliku2))
+                        {
+                            
+                            lista.Add(liniePliku1.Replace('\t',' '));
+                            
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllLines(NazwaPliku3, lista);
+        }
+
+        public void WysczyscListe()
+        {
+            lista.Clear();
+        }
+    }
+}
